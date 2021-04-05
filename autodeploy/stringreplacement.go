8082b1c1dc0d738aaf225c766ae716486d843d06
Arun Kutty <arunkutty@Aruns-MacBook-Pro.local>
2021-04-05 21:51:37 +0530

    Initial commit

---
+++ autodeploy/stringreplacement.go
@@ -0,0 +1,34 @@
+package autodeploy
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var substrings = []string{"Google","Oracle","Microsoft","Amazon","google","oracle","microsoft","amazon","deloitte"}
+
+//CopyRight adds a copyright symbol if the above strings are encountered
+func CopyRightWord(w http.ResponseWriter, r *http.Request) {
+	queryParamDisplayHandler(w, r)
+}
+
+
+func checkSubstrings(inputString string) string {
+	for _, sub := range substrings {
+		if strings.Contains(inputString, sub) {
+			inputString = strings.ReplaceAll(inputString,sub,sub+"©")
+		}
+	}
+	return inputString
+}
+
+func queryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
+	inputSentence := req.FormValue("sentence")
+	if inputSentence != "" {
+		fmt.Fprint(res, "New Copyrighted Sentence: "+checkSubstrings(inputSentence))
+	} else {
+		fmt.Fprint(res, "New Copyrighted Sentence: "+"The Input sentence is empty")
+	}
+}
+
